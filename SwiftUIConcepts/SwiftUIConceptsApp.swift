b5288df7ab3ea5a2bccbd6a8c2d56bd02926f1dc
Eduard Elenskiy <eelenskiy@ozon.ru>
2024-07-20 03:03:20 +0300

    Initial Commit

---
+++ SwiftUIConcepts/SwiftUIConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConceptsApp.swift
+//  SwiftUIConcepts
+//
+//  Created by eelenskiy on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
