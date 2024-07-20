8567432a320eafc54b4d65bcc6cf740d046fb3b6
Eduard Elenskiy <eelenskiy@ozon.ru>
2024-07-20 19:06:22 +0300

    Created project

---
+++ SwiftUIConcepts/Application/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  SwiftUIConcepts
+//
+//  Created by eelenskiy on 20.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
